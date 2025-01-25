e7d33edc08f939393b700d9a8d5ebdbaf4afdec2
krittika575 <krittikakritti10@gmail.com>
2025-01-25 20:42:51 +0530

    first

--- models/users.js
+++
@@ -1,9 +0,0 @@
-var mbd=require('mongoose')
-var UserSchema=mbd.Schema({
-    FirstName:String,
-    LastName:String,
-    Email:String,
-    Password:String
-})
-var user_Schema=mbd.model("log",UserSchema)
-module.exports=user_Schema;
